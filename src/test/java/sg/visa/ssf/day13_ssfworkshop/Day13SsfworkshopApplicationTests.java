15441c4e0fc7add36dc8fd16f6b9fd6751ec373d
Hamukaze <maplecow@gmail.com>
2023-05-17 13:36:56 +0800

    Initial commit

---
+++ src/test/java/sg/visa/ssf/day13_ssfworkshop/Day13SsfworkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.visa.ssf.day13_ssfworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day13SsfworkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
